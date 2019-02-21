cb584d056a105cf70f85b7e979cb67b1cb8e8663
Ivo Georgiev <ivo@strem.io>
2019-02-22 00:37:26 +0200

    random selection

--- dice.js
+++ dice.js
@@ -7,4 +7,26 @@ const pool = [
 	'https://stremio-imdb-list.now.sh/ls028057361/catalog/movie/imdb-movie-list.json',
 	'https://stremio-imdb-list.now.sh/ls041868378/catalog/movie/imdb-movie-list.json',
 	'https://stremio-imdb-list.now.sh/ls045252260/catalog/movie/imdb-movie-list.json',
-]
+];
+
+const shuffleArray = arr => arr
+	.map(a => [Math.random(), a])
+	.sort((a, b) => a[0] - b[0])
+	.map(a => a[1])
+
+const MAX_REQUESTS = 3
+const requests = shuffleArray(pool)
+	.slice(0, MAX_REQUESTS)
+	.map(url => fetch(url).then((resp) => resp.json()))
+
+// @TODO consider ignoring errors if only some of the requests failed
+Promise.all(requests)
+	.then((everything) => {
+		const all = everything.map(x => Array.isArray(x.metas) ? x.metas : [])
+			.reduce((a, b) => a.concat(b), [])
+		const shuffled = shuffleArray(all)
+		const rand = shuffled[0]
+		
+		document.body.style.background = `url('https://images.metahub.space/background/medium/${rand.id}/img')`
+	})
+
