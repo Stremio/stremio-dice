de06d295ae7c21d903771b9cafdb62674918d7c7
Ivo Georgiev <ivo@strem.io>
2019-02-22 00:25:32 +0200

    pool added

---
+++ dice.js
@@ -0,0 +1,10 @@
+const pool = [
+	'https://v3-cinemeta.strem.io/catalog/movie/top/skip=0.json',
+	'https://v3-cinemeta.strem.io/catalog/movie/top/skip=1.json',
+	'https://v3-cinemeta.strem.io/catalog/movie/top/skip=2.json',
+	'https://v3-cinemeta.strem.io/catalog/movie/top/skip=4.json',
+	'https://v3-cinemeta.strem.io/catalog/movie/top/skip=5.json',
+	'https://stremio-imdb-list.now.sh/ls028057361/catalog/movie/imdb-movie-list.json',
+	'https://stremio-imdb-list.now.sh/ls041868378/catalog/movie/imdb-movie-list.json',
+	'https://stremio-imdb-list.now.sh/ls045252260/catalog/movie/imdb-movie-list.json',
+]
